c4e1d5178b222b3e6439b90da3b074441b3db3ec
louizavanherp <louiza.vanherp@gmail.com>
2018-06-06 10:35:32 +0200

    fixed delete and eaten btn

--- includes/editDelete.inc.php
+++ includes/editDelete.inc.php
@@ -1,6 +1,6 @@
 <div class="editBox__delete editBox">
     <div class="editBox__delete__content editBox__content">
-        <a class='editBox__delete__content__delete editBox__content__btn' href="?id=<?php echo $productId; ?>&delete=<?php echo $stockId; ?>">Weggooien</a>
+        <a class='editBox__delete__content__delete editBox__content__btn' href="?id=<?php echo $_GET['id']; ?>&delete=<?php echo $_GET['stockId']; ?>">Weggooien</a>
         <a class='editBox__delete__content__cancel editBox__content__cancel' href="#">Annuleren</a>
     </div>
   </div>
